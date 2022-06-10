1abcc1ba40cc21b42355416912f44ecb246e3c35
Lucemans <luc@lucemans.nl>
2022-06-10 14:56:24 +0200

    Update fonts

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,4 @@
 import { render } from "react-dom";
 import { App } from "./App";
 
-import "./styles/tailwind.css";
-import "./styles/global.css";
-
 render(<App />, document.getElementById("root"));
