535e06daeea76a6eb79258e721208075940d9cb5
Lucemans <luc@lucemans.nl>
2022-06-11 13:34:00 +0200

    temp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ export const App = () => {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/admin" element={<Admin />} />
-                    <Route path="/grant/:id" element={<Grant />} />
+                    <Route path="/grant/:slug/:id" element={<Grant />} />
                 </Routes>
             </div>
         </BrowserRouter>
