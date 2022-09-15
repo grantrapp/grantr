c8cb49e3029b1cc92cbf4e6c50422555497ead21
Svemat01 <jakob.helgesson@gmail.com>
2022-09-15 22:01:42 +0200

    Add admin menu for frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,8 @@ import {
 import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
 import { publicProvider } from 'wagmi/providers/public';
 import { WIP } from './WIP';
+import { AdminTagEdit } from './admin/tags/AdminTagEdit';
+import { Tags } from './admin/tags/Tags';
 
 const { chains, provider } = configureChains(
     [chain.mainnet],
@@ -57,10 +59,16 @@ export const App = () => {
                                     path="/admin/:id/edit"
                                     element={<AdminPostEdit />}
                                 />
+                                <Route path="/admin/tags" element={<Tags />} />
                                 <Route
-                                    path="/grant/:id"
-                                    element={<Grant />}
+                                    path="/admin/tags/new"
+                                    element={<AdminTagEdit isNew />}
                                 />
+                                <Route
+                                    path="/admin/tags/:id/edit"
+                                    element={<AdminTagEdit />}
+                                />
+                                <Route path="/grant/:id" element={<Grant />} />
                             </>
                         ) : (
                             <Route path="/" element={<WIP />} />
