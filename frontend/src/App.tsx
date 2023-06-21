30598ac8f5376d979a344ae5a040c5cd5c7ab453
Jonatan <jonatan@jontes.page>
2023-06-21 19:48:48 +0200

    Add a Backend down screen closing #26 (#28)
    * Introduce backend down screen closing #26
    
    * Remove unnecessary useEffect
    
    * Use actual URL
    
    * Readd useEffect because yes
    
    * Fixing @lucemans changes
    
    * Fix one little eslint oopsie, more to go
    
    * Misc changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,22 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-import { Admin } from './admin/Admin';
-import { AdminPostEdit } from './admin/AdminPostEdit';
-import { Grant } from './grant/Grant';
-import { Home } from './home/Home';
-
 import {
     darkTheme,
     getDefaultWallets,
     RainbowKitProvider,
 } from '@rainbow-me/rainbowkit';
-
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
 import { publicProvider } from 'wagmi/providers/public';
-import { WIP } from './WIP';
+
+import { Admin } from './admin/Admin';
+import { AdminPostEdit } from './admin/AdminPostEdit';
 import { AdminTagEdit } from './admin/tags/AdminTagEdit';
 import { Tags } from './admin/tags/Tags';
+import { Down } from './Down';
+import { Grant } from './grant/Grant';
+import { Home } from './home/Home';
+import { GLOBALS } from './index';
+import { WIP } from './WIP';
 
 const { chains, provider } = configureChains(
     [chain.mainnet],
@@ -33,9 +34,30 @@ const wagmiClient = createClient({
     provider,
 });
 
-const maintenance = false;
-
 export const App = () => {
+    const [status, setStatus] = useState<
+        'up' | 'maintenance' | 'unexpectedDown'
+    >('up');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(GLOBALS.API_URL);
+
+                if (response.status === 503) {
+                    setStatus('maintenance');
+                } else if (response.status !== 200) {
+                    setStatus('unexpectedDown');
+                }
+            } catch (error) {
+                // eslint-disable-next-line prettier/prettier
+                console.error('Could not fetch maintenance status, erroring: ' + error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <WagmiConfig client={wagmiClient}>
             <RainbowKitProvider
@@ -49,31 +71,47 @@ export const App = () => {
             >
                 <BrowserRouter>
                     <Routes>
-                        {!maintenance || localStorage.getItem('luc-debug') ? (
-                            <>
-                                <Route path="/" element={<Home />} />
-                                <Route path="/admin" element={<Admin />} />
-                                <Route
-                                    path="/admin/new"
-                                    element={<AdminPostEdit isNew />}
-                                />
-                                <Route
-                                    path="/admin/:id/edit"
-                                    element={<AdminPostEdit />}
-                                />
-                                <Route path="/admin/tags" element={<Tags />} />
-                                <Route
-                                    path="/admin/tags/new"
-                                    element={<AdminTagEdit isNew />}
-                                />
-                                <Route
-                                    path="/admin/tags/:id/edit"
-                                    element={<AdminTagEdit />}
-                                />
-                                <Route path="/grant/:id" element={<Grant />} />
-                            </>
+                        {status === 'unexpectedDown' ? (
+                            <Route path="/" element={<Down />} />
                         ) : (
-                            <Route path="/" element={<WIP />} />
+                            <>
+                                {status !== 'maintenance' ||
+                                localStorage.getItem('luc-debug') ? (
+                                    <>
+                                        <Route path="/" element={<Home />} />
+                                        <Route
+                                            path="/admin"
+                                            element={<Admin />}
+                                        />
+                                        <Route
+                                            path="/admin/new"
+                                            element={<AdminPostEdit isNew />}
+                                        />
+                                        <Route
+                                            path="/admin/:id/edit"
+                                            element={<AdminPostEdit />}
+                                        />
+                                        <Route
+                                            path="/admin/tags"
+                                            element={<Tags />}
+                                        />
+                                        <Route
+                                            path="/admin/tags/new"
+                                            element={<AdminTagEdit isNew />}
+                                        />
+                                        <Route
+                                            path="/admin/tags/:id/edit"
+                                            element={<AdminTagEdit />}
+                                        />
+                                        <Route
+                                            path="/grant/:id"
+                                            element={<Grant />}
+                                        />
+                                    </>
+                                ) : (
+                                    <Route path="/" element={<WIP />} />
+                                )}
+                            </>
                         )}
                     </Routes>
                 </BrowserRouter>
